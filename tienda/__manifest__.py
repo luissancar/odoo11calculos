1f4d312eabd76e6e11d3d202e9b689249b7b2c7e
luis jose sanchez carrasco <luissancar@yahoo.es>
2020-02-27 17:36:14 +0100

    Update __manifest__.py

--- tienda/__manifest__.py
+++ tienda/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Plugin Tienda',
     'description': 'Modelo básico con elementos de una tienda',
-    'author': 'Ruben Segura',
+    'author': 'ies',
     'depends': ['mail'],
     'application': True,
     'data': ['views/proveedores.xml', 'views/clientes.xml', 'views/articulos.xml', 'views/ventas.xml']
